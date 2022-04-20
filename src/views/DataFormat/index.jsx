d2554c5cdca585f2a6d3c8591a337c2a110acd59
Your Name <you@example.com>
2022-04-20 18:38:56 +1200

    table formatting

--- src/views/DataFormat/index.jsx
+++ src/views/DataFormat/index.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
-import { useApi, Provider } from '@/components/Provider';
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
 
+import { useApi, Provider } from '@/components/Provider';
 
 export const DataFormat = () => {
     return (
@@ -16,14 +21,30 @@ const Data = () => {
     const [, { queryGeo }] = useApi();
     useEffect(() => {
         queryGeo({ set: setGeolocation })
+
     }, []);
 
     return (
-        <>
-            {geolocation.data && geolocation.data.map((item, i) =>
-                <p key={i}>{item.id} / {item.ipAddress} / {item.country}</p>
-            )}
-        </>
+        <TableContainer>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableBody>
+                    {geolocation.data && geolocation.data.map((item, i) =>
+                        <TableRow key={i}>
+                            <TableCell>
+                                {item.id}
+                            </TableCell>
+                            <TableCell>
+                                {item.ipAddress}
+                            </TableCell>
+                            <TableCell>
+                                {item.country}
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table >
+        </TableContainer>
+
     )
 }
 export default DataFormat; 
