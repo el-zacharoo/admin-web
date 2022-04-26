4c905d5f7b21889ee8e8124f8b202872e8dee4a5
Your Name <you@example.com>
2022-04-27 11:43:08 +1200

    added user button

--- src/Viewport/Header.jsx
+++ src/Viewport/Header.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Link from '@mui/material/Link';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Avatar } from '@mui/material';
 
 
 
 export const Header = () => {
-
+    const { user } = useAuth0()
+    console.log(user)
 
     return (
         <AppBar elevation={0} color="primary" position="relative" sx={{ padding: 1 }}>
@@ -18,6 +22,7 @@ export const Header = () => {
                 <Link underline="none" sx={{ display: 'flex', '& svg': { fontSize: '2rem', mr: 1 }, alignItems: 'center', color: 'info.main' }} component={RouterLink} to="/">
                     <Typography variant="h4">{import.meta.env.VITE_APP_NAME}</Typography>
                 </Link>
+                <Button endIcon={<Avatar sx={{ width: 24, height: 24 }} src={user.picture} alt="Pic" />} color="info">{user.nickname}</Button>
             </Toolbar>
         </AppBar>
     )
